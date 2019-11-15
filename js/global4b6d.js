ab2194dd96c893211879c87b941204a6350e791e
victorbruce <victorbruce82@gmail.com>
2019-11-15 13:08:36 +0000

    navigate from bottom menus

--- js/global4b6d.js
+++ js/global4b6d.js
@@ -8,5 +8,6 @@ const navigateToMenu = function(path){
 const menus = Array.from(document.getElementsByClassName('menu-middle'));
 menus.forEach(m => m.addEventListener('click', () => navigateToMenu(m.href)));
 
-const bottomMenus = Array.from(document.getElementsByClassName('menu-item'));
+let bottomMenus = Array.from(document.getElementsByClassName('menu-item'));
+bottmMenus = bottomMenus.splice((bottomMenus.length - 2 ), 1);
 bottomMenus.forEach(m => m.addEventListener('click', () => navigateToMenu(m.href)));
